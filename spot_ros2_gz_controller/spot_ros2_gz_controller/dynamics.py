ba08cfa5d9cf38830f30c9f96ef4ffc83e857e71
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-12-02 23:37:03 +0000

    test pinocchio forward kinematics

--- spot_ros2_gz_controller/spot_ros2_gz_controller/dynamics.py
+++ spot_ros2_gz_controller/spot_ros2_gz_controller/dynamics.py
@@ -1,14 +1,25 @@
-import pinocchio
+import sys
+import pinocchio as pin
+from pinocchio.visualize import MeshcatVisualizer
 from pathlib import Path
+
 from ament_index_python.packages import get_package_share_directory
 
 def main():
     package_share_dir = get_package_share_directory('spot_ros2_description')
-    pinocchio_model_dir = Path(package_share_dir) / "models" / "spot"
-    urdf_filename = pinocchio_model_dir / "model.urdf"
+    model_dir = Path(package_share_dir) / "models" / "spot"
+    urdf_filename = model_dir / "model.urdf"
     
-    model = pinocchio.buildModelFromUrdf(str(urdf_filename))
+    model, collision_model, visual_model = pin.buildModelsFromUrdf(str(urdf_filename))
     print("model name: " + model.name)
+
+    data = model.createData()
+    # Sample a random configuration
+    q = pin.randomConfiguration(model)
+    print(f"q: {q.T}")
+
+    # Perform the forward kinematics over the kinematic tree
+    pin.forwardKinematics(model, data, q)
 
 if __name__ == '__main__':
     main()
